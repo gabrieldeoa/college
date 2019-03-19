d55f16f15ac188a79f684b8eb0cb81ab46ee2246
Gabriel Araujo <gabriel.araujo@rgsistemas.com.br>
2019-03-19 20:53:41 -0300

    add resume of algoritmn

--- structured-programming/src/exercises/list-3/list-3-3.c
+++ structured-programming/src/exercises/list-3/list-3-3.c
@@ -1,3 +1,10 @@
+/*
+Programe uma função para receber em seus parâmetros dois números inteiros.
+Sua função deve indicar se o primeiro parâmetro é divisível pelo segundo.
+Em seguida, chame esta função a partir de um módulo main para testar seu programa.
+Neste programa, informe se um número fornecido pelo usuário é par ou ímpar.
+*/
+
 #include <stdio.h>
 
 int divisivel(int x, int y) {
