f9a30da60f4bc53fa9086ea919f8355a0281cdb4
Gabriel de O. Araujo <gabriel@digitalm3.com.br>
2019-04-14 18:50:24 -0300

    Add exercise 5-4 recursive and update the iterative

--- structured-programming/src/exercises/list-5/list-5-4-recursive.c
+++ structured-programming/src/exercises/list-5/list-5-4-recursive.c
@@ -3,21 +3,18 @@ Implemente uma função para calcular o fatorial de um número.
 O fatorial de 0 é 1, por definição.
 Para qualquer outro número inteiro positivo n, o seu fatorial é dado por n x (n-1) x (n-2) x ... x 3 x 2 x 1.
 Em seguida, chame este módulo a partir de um módulo main para testar seu programa.
+===> RECURSIVO <===
 */
 
 #include <stdio.h>
 
-
-int fatorial(int n) {
-    int fatorial = 1;
-
-    if(n == 0) return fatorial;
-
-    for(int i=1; i <= n; i++){
-        fatorial = fatorial * i;
-    }
-
-    return fatorial;
+int fatorialRec(int n) {
+    //valor de parada
+    if(n == 0)
+        return 1; // Elementro neutro multiplicacao
+    //caso nao, multiplique N e decremente
+    else
+        return n * fatorialRec(n-1);
 }
 
 main() {
@@ -29,7 +26,7 @@ main() {
         printf("\nForneca um numero para calcular seu fatorial: ");
         scanf("%d", &n);
 
-        printf("\nO Fatorial de %d e %d.", n, fatorial(n));
+        printf("\nO Fatorial de %d e %d.", n, fatorialRec(n));
 
         printf("\n\nDeseja Continuar [1 para sim e 2 para nao] ? ");
         scanf("%d", &r);
