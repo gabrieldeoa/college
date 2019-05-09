588ea30a8dd9c05393199e448ae97d1d4da6965e
Gabriel de O Araujo <gabrielaraujo.dev@gmail.com>
2019-05-08 23:40:32 -0300

    list seven exercises 1 and 2

---
+++ structured-programming/src/exercises/list-7/list-7-1.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+enum TSexo {masculino, feminino};
+
+enum TEstadoCivil {
+    solteiro,
+    casado,
+    viuvo,
+    separado,
+    desquitado
+};
+
+typedef enum TSexo Sexo;
+typedef enum TEstadoCivil EstadoCivil;
+
+char * TSexoParaTexto(Sexo sexo) {
+    switch(sexo) {
+        case masculino: return "masculino";
+        case feminino: return "feminino";
+        default: "sexo invalido";
+    }
+}
+
+char * TEstadoCivilParaTexto(EstadoCivil estadoCivil) {
+    switch(estadoCivil) {
+        case solteiro: return "solteiro";
+        case casado: return "casado";
+        case viuvo: return "viuvo";
+        case separado: return "separado";
+        case desquitado: return "desquitado";
+        default: "estado civil inválido";
+    }
+}
+
+main() {
+    int s, es;
+    Sexo sexo;
+    EstadoCivil estadoCivil;
+
+    printf("Forneca o seu sexo [0-masculino ou 1-feminino");
+    scanf("%d", &s);
+    sexo = (Sexo)s;
+
+    printf("Forneca o seu sexo [0-solteiro, 1-casado, 2-viuvo, 3-separdo, 4-desquitado");
+    scanf("%d", &es);
+    estadoCivil = (EstadoCivil)es;
+
+    printf("Voce e do sexo %s e seu estado civil e %s",
+        TSexoParaTexto(sexo),
+        TEstadoCivilParaTexto(estadoCivil)
+    );
+}
---
+++ structured-programming/src/exercises/list-7/list-7-2.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef enum TTipoEndereco {
+    Rua,
+    Avenida,
+    Largo,
+    Jardim,
+    Estrada
+} TipoEndereco;
+
+typedef struct TEndereco {
+    TipoEndereco tipo;
+    char * logradouro, * cidade;
+    char estado[2];
+    int numero, cep;
+} Endereco;
+
+char * TTipoEnderecoParaTexto(TipoEndereco tipo) {
+    switch(tipo) {
+        case Rua: return "Rua";
+        case Avenida: return "Avenida";
+        case Largo: return "Largo";
+        case Jardim: return "Jardim";
+        case Estrada: return "Estrada";
+        default: "Tipo de endereco inválido";
+    }
+}
+
+main() {
+    int te;
+    Endereco * endereco = (Endereco *)malloc(sizeof(Endereco));
+    endereco->cidade = (char*)malloc(sizeof(char));
+    endereco->logradouro = (char*)malloc(sizeof(char));
+
+    printf("\nLogradouro: ");
+    gets(endereco->logradouro);
+
+    printf("\nCidade: ");
+    gets(endereco->cidade);
+
+    printf("\nEstado-UF: ");
+    gets(endereco->estado);
+
+    printf("\nTipo: ");
+    printf("\n0 - Rua\n1 - Avenida\n2 - Largo\n3 - Jardim\n4 - Estrada\nForneca o numero do tipo de endereco:");
+    scanf("%d", &te);
+    endereco->tipo = (TipoEndereco)te;
+
+    printf("\nCEP: ");
+    scanf("%d", &endereco->cep);
+
+    printf("\nNumero: ");
+    scanf("%d", &endereco->numero);
+
+    printf("\n\nSeu endereco e: %s %s nº %d, %d %s - %s",
+        TTipoEnderecoParaTexto(endereco->tipo),
+        endereco->logradouro,
+        endereco->numero,
+        endereco->cep,
+        endereco->cidade,
+        endereco->estado
+    );
+
+    free(endereco->logradouro);
+    free(endereco->cidade);
+    free(endereco);
+}
