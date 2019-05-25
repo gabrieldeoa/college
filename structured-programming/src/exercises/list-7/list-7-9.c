26001d27e32c425f1447b993a85384723e6f2860
Gabriel de O Araujo <gabrielaraujo.dev@gmail.com>
2019-05-25 10:51:07 -0300

    finishing exercise nine

--- structured-programming/src/exercises/list-7/list-7-9.c
+++ structured-programming/src/exercises/list-7/list-7-9.c
@@ -2,15 +2,14 @@
 #include <stdlib.h>
 
 typedef struct TNumeroRacional {
-    int n;
-    int d;
+    float n;
+    float d;
 } NumeroRacional;
 
 NumeroRacional soma(NumeroRacional n1, NumeroRacional n2) {
     NumeroRacional resposta;
     resposta.n = (n1.n * n2.d) + (n2.n * n1.d);
     resposta.d = n1.d * n2.d;
-    printf("%f %f", resposta.d, resposta.n);
 
     return resposta;
 }
@@ -54,6 +53,17 @@ main () {
 
     printf("o denominador de y: ");
     scanf("%f", &n2.d);
+
     res = soma(n1, n2);
+    printf("\n%.2f/%.2f + %.2f/%.2f = %.2f/%.2f", n1.n, n1.d, n2.n, n2.d, res.n, res.d);
+
+    res = subtracao(n1, n2);
+    printf("\n%.2f/%.2f - %.2f/%.2f = %.2f/%.2f", n1.n, n1.d, n2.n, n2.d, res.n, res.d);
+
+    res = multiplicacao(n1, n2);
+    printf("\n%.2f/%.2f x %.2f/%.2f = %.2f/%.2f", n1.n, n1.d, n2.n, n2.d, res.n, res.d);
+
+    res = divisao(n1, n2);
+    printf("\n%.2f/%.2f / %.2f/%.2f = %.2f/%.2f", n1.n, n1.d, n2.n, n2.d, res.n, res.d);
 
 }
