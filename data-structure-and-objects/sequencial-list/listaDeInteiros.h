d3aa8317a8349a5272d27b88008ec193401419e1
Gabriel de O Araujo <gabrielaraujo.dev@gmail.com>
2019-08-25 23:47:29 -0300

    new concept - sequencial list

---
+++ data-structure-and-objects/sequencial-list/listaDeInteiros.h
@@ -0,0 +1,132 @@
+#ifndef LISTA_DE_INTEIROS
+#define LISTA_DE_INTEIROS
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define LISTA_NAO_ALOCADA = -1;
+#define ELEMENTO_NAO_EXISTENTE = -2;
+#define ELEMENTO_EXISTENTE = -3;
+#define LISTA_CHEIA = -4;
+#define LISTA_VAZIA = -5;
+#define SUCESSO = -6;
+
+int** criarLista(int tam) {
+    int **lista, i;
+
+    lista = calloc(tam, sizeof(int*[tam]));
+
+    return lista;
+}
+
+int tamanhoDaLista(int **lista, int tam) {
+    int i;
+
+    if(lista == NULL)
+        return LISTA_NAO_ALOCADA;
+
+    for(i=0; i < tam; i++)
+        if(lista[i] == NULL)
+            return i;
+    
+    return tam;
+}
+
+int posicaoDoElemento(int elemento, int** lista, int tam) {
+    int i, totalElementos;
+
+    if(lista == NULL)
+        return LISTA_NAO_ALOCADA;
+
+    totalElementos = tamanhoDaLista(lista, tam);
+
+    for(i=0; i < totalElementos; i++)
+        if(*(lista[i]) == elemento)
+            return i;
+    
+    return ELEMENTO_NAO_EXISTENTE;
+}
+
+int inserirElemento(int elemento, int** lista, int tam) {
+    int i, totalElementos, posicaoElemento;
+
+    totalElementos = tamanhoDaLista(lista, tam);
+
+    if(totalElementos >= tam )
+        return LISTA_CHEIA;
+    
+    posicaoElemento = posicaoDoElemento(elemento, lista, tam);
+
+    if(posicaoElemento != ELEMENTO_NAO_EXISTENTE)
+        return ELEMENTO_EXISTENTE;
+    
+    lista[totalElementos] = malloc(sizeof(int));
+
+    *(lista[totalElementos]) = elemento;
+
+    return SUCESSO;
+
+
+}
+
+int removerElemento(int elemento, int** lista, int tam) {
+    int i, posicao, totalElementos;
+
+    totalElementos = tamanhoDaLista(lista, tam);
+    posicao = posicaoDoElemento(elemento, lista, tam);
+
+    if(totalElementos <= 0)
+        return LISTA_VAZIA;
+
+    if(posicao == ELEMENTO_NAO_EXISTENTE)
+        return ELEMENTO_NAO_EXISTENTE;
+
+    free(lista[posicao]);
+
+    for(i = posicao; i < totalElementos-1; i++)
+        lista[i] = lista[i + 1];
+    
+    lista[i] = NULL;
+
+    return SUCESSO;
+}
+
+void exibirLista(int **lista, int tam) {
+    int i;
+
+    for(i = 0; i < tam; i++)
+        if(lista[i] != NULL)
+            printf("%d|", *(lista[i]));
+        else
+            printf("N|");
+    printf("\n");
+}
+
+int** lerLista(int tam) {
+    int i, elemento, ** lista;
+
+    lista = criarLista(tam);
+
+    for(i=0; i < tam; i++) {
+        printf("LISTA[%d] = ", i);
+        scanf("%d", &elemento);
+
+        if(elemento == '\0')
+            return lista;
+        
+        inserirElemento(elemento, lista, tam);
+    }
+    
+    return lista;
+}
+
+void destruirFila(int** lista, int tam) {
+    int i;
+
+    for(i=0; i < tam; i++)
+        free(lista[i]);
+
+    free(lista);
+}
+
+#endif
